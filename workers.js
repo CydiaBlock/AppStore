8e91ba2f263434a382fe4ac007be39c787e18f05
dopaemon <polarisdp@gmail.com>
2026-08-04 13:09:45 +0700

    Proxy package indexes through Worker
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- workers.js
+++ workers.js
@@ -1,5 +1,9 @@
 const REPO = "CydiaBlock/AppStore";
 const RELEASES = new Set(["game", "application"]);
+const INDEXES = new Set(["Packages", "Packages.gz"]);
+
+const rawFile = (name) =>
+  `https://raw.githubusercontent.com/${REPO}/main/${name}`;
 
 export default {
   async fetch(request) {
@@ -11,6 +15,18 @@ export default {
     }
 
     const path = new URL(request.url).pathname.split("/").filter(Boolean);
+    if (path.length === 1 && INDEXES.has(path[0])) {
+      return fetch(new Request(rawFile(path[0]), request), { redirect: "follow" });
+    }
+
+    if (
+      path.length === 2 &&
+      RELEASES.has(path[0]) &&
+      INDEXES.has(path[1])
+    ) {
+      return fetch(new Request(rawFile(path[1]), request), { redirect: "follow" });
+    }
+
     if (path.length < 2 || !RELEASES.has(path[0])) {
       return new Response("Not found", { status: 404 });
     }
