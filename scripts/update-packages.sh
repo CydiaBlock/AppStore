d91177ee31c5b8df7f59b665a2f6a72020ebc1a7
dopaemon <polarisdp@gmail.com>
2026-08-04 19:38:12 +0700

    Replace stale metadata for reused package filenames
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -28,29 +28,30 @@ fi
 
 dpkg-scanpackages -m work/deb /dev/null > work/new-Packages
 rewrite_filenames work/new-Packages > work/new-Packages.direct
-awk '/^Filename: / { print $2 }' Packages > work/known-files
-awk -v RS='' -v ORS='\n\n' -v known=work/known-files '
-  BEGIN {
-    while ((getline file < known) > 0) seen[file] = 1
-    close(known)
-  }
-  {
-    file = ""
-    count = split($0, lines, "\n")
+awk -v RS='' -v ORS='\n\n' '
+  function filename(block, lines, count, i, file) {
+    count = split(block, lines, "\n")
     for (i = 1; i <= count; i++) {
       if (lines[i] ~ /^Filename: /) {
         file = lines[i]
         sub(/^Filename: /, "", file)
+        return file
       }
     }
-    if (file != "" && !seen[file]) {
-      print $0
-      seen[file] = 1
-    }
+    return ""
   }
-' work/new-Packages.direct > work/unique-Packages
-if [ -s work/unique-Packages ]; then
-  [ ! -s Packages ] || printf '\n' >> Packages
-  cat work/unique-Packages >> Packages
-fi
+  FNR == NR {
+    file = filename($0)
+    if (file != "") replacement[file] = $0
+    next
+  }
+  {
+    file = filename($0)
+    if (!(file in replacement)) print $0
+  }
+  END {
+    for (file in replacement) print replacement[file]
+  }
+' work/new-Packages.direct Packages > work/merged-Packages
+mv work/merged-Packages Packages
 gzip --keep --force --best Packages
